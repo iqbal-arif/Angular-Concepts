5f2bde288fc37f8890b15815006d51f28620fa75
Arif Iqbal <arif_iqbal@yahoo.com>
2023-02-17 20:49:03 -0600

    modified:  1-components/src/app/app.component.html modified:   1-components/src/app/app.component.ts

--- 1-components/src/app/app.component.ts
+++ 1-components/src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, ViewChild } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  ViewChildren,
+  AfterViewInit,
+  QueryList,
+} from "@angular/core";
 import { COURSES } from "../db-data";
 import { CourseCardComponent } from "./course-card/course-card.component";
 import { Course } from "./model/course";
@@ -8,13 +15,14 @@ import { Course } from "./model/course";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   // Listing Complete Course Model for the App Template
   courses = COURSES;
   // This model was done for displaying individual courses for the App Template HTML
   // coreCourse = COURSES[0];
   // rxjsCourse = COURSES[1];
   // ngrxCourse = COURSES[2];
+  /*
   // Title for Pipes
   title = COURSES[1].description;
   // Price for Pipes
@@ -25,7 +33,7 @@ export class AppComponent {
   course = COURSES[1];
   // Start Date for Pipes
   startDate = new Date(2023, 1, 1);
-
+  */
   // Child Reference in App Component
   // A Template Query Mechanism:  Query Component Type
   // @ViewChild(CourseCardComponent) Querying Component by Class Name
@@ -38,12 +46,19 @@ export class AppComponent {
   // Querying HTML Element Type
   @ViewChild("container") // Querying by plain HTML Template Reference
   containerDiv: ElementRef; // Handles native DOM elements
+  // Querying Multiple Card Reference
+  @ViewChildren(CourseCardComponent)
+  cards: QueryList<CourseCardComponent>;
 
+  constructor() {}
+  ngAfterViewInit() {
+    console.log(this.cards.first);
+  }
   // Click Event Handler
   onCourseSelected(course: Course) {
     console.log("App Component - Click event Bubbled....", course);
     // Querying Card by Component Reference
-    console.log(this.card);
+    console.log(this.cards);
     // Querying DIV by HTML Reference
     console.log("containerDiv", this.containerDiv);
   }
