1916351f9fcac32007e45eba5b82523e99237dec
Arif Iqbal <arif_iqbal@yahoo.com>
2023-02-27 12:20:10 -0600

    modified:   3-services/Angular-Services-README modified:   3-services/src/app/app.component.ts modified:   3-services/src/app/services/courses.service.ts

--- 3-services/src/app/app.component.ts
+++ 3-services/src/app/app.component.ts
@@ -40,7 +40,7 @@ export class AppComponent implements OnInit {
   // L40: Define private service to utilize HTTP Client Services
   // L42: Define coursesService:CoursesService with type annotation to inject an instance of course
   constructor(
-    private http: HttpClient,
+    // /*private http: HttpClient, */ This is not needed since it is called in service file
     private coursesService: CoursesService
   ) {}
 
@@ -49,19 +49,26 @@ export class AppComponent implements OnInit {
   // L40:This ngOnInit method is called by Angular Framework
   ngOnInit() {
     // L42: Logging the value of courseService
-    console.log(this.coursesService);
+    // console.log(this.coursesService);
     // L40: Adding parameters to Http get request via Angular HttpParams utility
-    // THE PARAMS PART IS NOT WORKING .CHECK IT OUT
-    const params = new HttpParams().set("page", "1").set("pageSize", "10");
+    /****L43: MOVED THIS ALL TO SERVICES TEMPLATE : This section is prior to L43 */
+    // MOVED
+    // const params = new HttpParams().set("page", "1").set("pageSize", "10");
+    // MOVED
 
     // L40: http.get gives unobservable. To display the content use .subscribe() method
-    this.http.get("/api/courses", { params }); //L40: Adding request parameters from params constant
+    // MOVED
+    // this.http.get("/api/courses", { params }); //L40: Adding request parameters from params constant
+    // MOVED
     // L41: Using Observable
-    this.courses$ = this.http.get<Course[]>("/api/courses", { params });
+    // this.courses$ = this.http.get<Course[]>("/api/courses", { params });
     /***ACTIVATE FOR LESSON 40 ****/
     // L40: This code is used to test the API connection and display it in console as array
     // L40:.subscribe((arg) => console.log(arg ));
     // .subscribe((courses) => (this.courses = courses));
     /***ACTIVATE FOR LESSON 40 ****/
+    /****L43: MOVED THIS ALL TO SERVICES TEMPLATE: This section is prior to L43  */
+    // L43:
+    this.courses$ = this.coursesService.loadCourses();
   }
 }
--- 3-services/src/app/services/courses.service.ts
+++ 3-services/src/app/services/courses.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Course } from "../model/course";
 
 @Injectable({
   providedIn: "root",
 })
 export class CoursesService {
-  constructor() {
-    // L42: Logging to verify that it is the only one instance available in all cards
-    console.log("creating CoursesServices");
+  // L43: Define private service to utilize HTTP Client Services
+  constructor(private http: HttpClient) {}
+  // L42: Logging to verify that it is the only one instance available in all cards
+  // L42: Activate to see the log
+  // console.log("creating CoursesServices");
+
+  // L42: Defining API with return type Observable of Course Array
+  // L42: Values emitted are going to be instances of array of Courses
+  loadCourses(): Observable<Course[]> {
+    const params = new HttpParams().set("page", "1").set("pageSize", "10");
+
+    this.http.get("/api/courses", { params });
+    return this.http.get<Course[]>("/api/courses", { params });
   }
 }
